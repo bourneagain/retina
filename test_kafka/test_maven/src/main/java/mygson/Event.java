996b086e412f5ecedfc0f1a958fc9c38f8a875a2
saikatroyc <rychwdh2@illinois.edu>
2015-03-24 03:45:21 -0500

    test kafka producer

---
+++ test_kafka/test_maven/src/main/java/mygson/Event.java
@@ -0,0 +1,31 @@
+package mygson;
+
+/**
+ * Created by saikat on 3/24/15.
+ */
+public class Event {
+    String eventType;
+    String eventWarn;
+    String eventError;
+    String eventCrash;
+    String eventClick;
+    Event(String type, String warn, String error, String crash, String click) {
+        this.eventType = type;
+        this.eventWarn = warn;
+        this.eventError = error;
+        this.eventCrash = crash;
+        this.eventClick = click;
+    }
+
+    Event(String type, String warn, String error, String crash) {
+        this.eventType = type;
+        this.eventWarn = warn;
+        this.eventError = error;
+        this.eventCrash = crash;
+    }
+
+    Event(String type, String click) {
+        this.eventType = type;
+        this.eventClick = click;
+    }
+}
