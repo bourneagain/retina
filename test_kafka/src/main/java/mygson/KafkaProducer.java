e36ff473eee7dcf90d50603782252e7621c2a72f
saikatroyc <rychwdh2@illinois.edu>
2015-03-24 04:38:36 -0500

    send multiple msgs

--- test_kafka/src/main/java/mygson/KafkaProducer.java
+++ test_kafka/src/main/java/mygson/KafkaProducer.java
@@ -14,15 +14,15 @@ import kafka.message.Message;
 import kafka.producer.SyncProducerConfig;
 
 public class KafkaProducer {
-
+    
     static String generateTimeStamp() {
         String timeStamp = new SimpleDateFormat("yyyy-MM-dd:HH:mm:ss").format(new Date());
         String result = timeStamp.substring(0, 10) + "T" + timeStamp.substring(11) + 'Z';
         return result;
     }
-
+    
     static Integer eventID = 0;
-
+    
     static String getEventID() {
         eventID++;
         return Integer.toString(eventID);
@@ -38,7 +38,7 @@ public class KafkaProducer {
         props.put("request.required.acks", "1");
         ProducerConfig config = new ProducerConfig(props);
         Producer<String, String> producer = new Producer<String, String>(config);
-
+        
         PhoneMetadata pm = new PhoneMetadata("1234", "Nexus5", "M8974", "LRX22C");
         AppMetadata a = new AppMetadata("app1234", "1.0");
         Event e =  new Event("Click", "type1:3 type2:4");
@@ -47,9 +47,20 @@ public class KafkaProducer {
         re.eventid = getEventID();
         Random rnd = new Random();
         String ip = "192.168.2." + rnd.nextInt(255);
-        String msg = re.toJSON();
-        System.out.println("produce to kafka queue:" + msg);
-        KeyedMessage<String, String> data = new KeyedMessage<String, String>("test", ip, msg);
-        producer.send(data);
+        //String msg = re.toJSON();
+        //System.out.println("produce to kafka queue:" + msg);
+        //KeyedMessage<String, String> data = new KeyedMessage<String, String>("test", ip, msg);
+        //producer.send(data);
+        
+        ArrayList<KeyedMessage<String, String>> data_array = new ArrayList<KeyedMessage<String, String>>();
+        
+        for (int i = 0; i < 10; i++) {
+            re.timestamp = generateTimeStamp();
+            re.eventid = getEventID();
+            String msg1 = re.toJSON();
+            System.out.println("produce to kafka queue:" + msg1);
+            data_array.add(new KeyedMessage<String, String>("test", ip, msg1));
+        }
+        producer.send(data_array);
     }
 }
