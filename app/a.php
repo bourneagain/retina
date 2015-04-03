d54429e2c7b6c801f930771ebe8cd8b9bf622276
bslearning <samderik@gmail.com>
2015-04-03 14:05:39 -0500

    live druid data periodic ajax working!

--- app/a.php
+++ app/a.php
@@ -3,13 +3,9 @@
 <head>
 <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="css/iThing.css" type="text/css" />
+<link rel="stylesheet" href="css/main.css" type="text/css" />
 </head>
-<style>
-    body { 
-    padding-top: 200px;
-    padding-left: 70px;
-    }
-</style>
+<title>RETINAA : Real Time App Analytics</title>
 <body>
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container">
@@ -18,7 +14,7 @@
 </nav>
 
 <div class="row">
-  <div class="col-md-4">
+  <div class="col-md-4" id='leftPane'>
     <table style="width:100%">
       <tr>
         <td>Query Type</td>
@@ -84,7 +80,7 @@
        </td>
        <td>
             <select id='aggField' class="aggDim">
-             <option>count</option>
+             <option>errorcount</option>
              <option>phoneimei</option>
              <option>appid</option>
        </td>
@@ -95,7 +91,7 @@
             <div class='filterDim'>
                 <div id='clonefilter1'>
                     <select id='filField1'>
-                     <option>country </option>
+                     <option>eventtype</option>
                      <option>phoneimei </option>
                      <option>appid </option>
                     </select>
@@ -109,24 +105,22 @@
       </tr>
       </table>
   </div>
-  <div class="col-md-8">
+<div id='container'></div>
+  <div class="col-md-8" id='rightPane'>
         Result : <input id='results' size='50'></input>
+    
 
 <table id="example" class="display" cellspacing="0" width="100%">
         <thead>
             <tr>
-                <th>Engine</th>
-                <th>hrowser</th>
-                <th>platform</th>
-                <th>version</th>
+                <th>app</th>
+                <th>errorcount</th>
             </tr>
         </thead>
 <tfoot>
             <tr>
-                <th>Engine</th>
-                <th>hrowser</th>
-                <th>platform</th>
-                <th>version</th>
+                <th>app</th>
+                <th>errorcount</th>
             </tr>
         </tfoot>
 <tbody>
@@ -139,7 +133,7 @@
 <br>
 <br>
 <div class="btn-group" role="group" aria-label="...">
-  <button type="button" id='query'  class="btn btn-default">Query</button>
+  <button type="button" id='viewlive'  class="btn btn-default">viewLiveData</button>
 </div>
 
 Sample Input : 
@@ -166,8 +160,8 @@ Sample Input :
 <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
 <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.min.js"></script>
 
-<script src="jQRangeSlider-min.js"></script>
-<script src="jQDateRangeSlider-min.js"></script>
+<script src="js/jQRangeSlider-min.js"></script>
+<script src="js/jQDateRangeSlider-min.js"></script>
 
 <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script> -->
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
@@ -175,21 +169,14 @@ Sample Input :
 <link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.5/css/jquery.dataTables.css">
 <!-- DataTables -->
 <script type="text/javascript" charset="utf8" src="//cdn.datatables.net/1.10.5/js/jquery.dataTables.js"></script>
+<script type="text/javascript" src="js/highcharts.js"></script>
 
 <script>
-$(document).ready(function() {
-    $('#btnAdd').hide();
-    // comment to have additional filter addition
-     $('#btnAdd').click(function() {
-            var num = $('.filterDim').length;
-            var newNum  = new Number(num + 1)
-            var newEl = $('#clonefilter'+num).clone().attr('id','clonefilter'+newNum);
-            newEl.find('#filField'+num).attr('id','filField'+newNum)
-            newEl.find('.input-group').find('#filFieldInput'+num).attr('id','filFieldInput'+newNum)
-            $('.filterDim').append(newEl);
-            //$('.filterDim').after(newEl);
-    });
-});
+
+var chart; // global
+var period = 30;
+
+
 //
 //var bounds = $("#slider").dateRangeSlider("option", "bounds");
 //$(document).ready(function(){
@@ -216,12 +203,115 @@ $("#slider").bind("valuesChanged", function(e, data){
   eax_date = data.values.max;
 });
 
+function formQuery(){
+// include the query map details depending on what live view is required to view
+}
 
+function requestData() {
+    queryMap =  {};
+    queryMap['queryType']="timeseries";
+    queryMap['dataSource']="test";
+    queryMap['granularity']="all";
+    var temp={};
+    queryMap["aggregations"] = [];
+    temp["type"]="longSum";
+    temp["fieldName"]="errorcount";
+    temp["name"]="errorcount";
+    queryMap["aggregations"].push(temp);
+	var maxd= new Date();
+	var mind = new Date();
+	mind.setSeconds(maxd.getSeconds() - period  );
+    var maxd_year = maxd.getFullYear();
+    var maxd_month = (maxd.getMonth()+1)%12;
+    var maxd_date = maxd.getDate();
+    var maxd_min= maxd.getMinutes();
+    var maxd_hour= maxd.getHours();
+    var maxd_second = maxd.getSeconds();
+    var mind_year = mind.getFullYear();
+    var mind_month = (mind.getMonth()+1)%12;
+    var mind_date = mind.getDate();
+    var mind_min= mind.getMinutes();
+    var mind_hour= mind.getHours();
+    var mind_second = mind.getSeconds();
+    var maxd_str = maxd_year+'-'+maxd_month+'-'+maxd_date+'T'+maxd_hour+':'+maxd_min+':'+maxd_second;
+    var mind_str = mind_year+'-'+mind_month+'-'+mind_date+'T'+mind_hour+':'+mind_min+':'+mind_second;
+    var intervals = mind_str+'/'+maxd_str;
+    queryMap["intervals"] = [];
+    queryMap["intervals"].push(intervals);
+    console.log(queryMap);
+    $.ajax({
+        type : "post",
+        url: 'live-server-data.php', 
+        data: {
+            "json" : JSON.stringify(queryMap)
+        },
+        success: function(point) {
+            // console.log("DATA"+data);
+            // data = data.slice(1, - 1);
+            // console.log("stringdata "+data);
+            // ajaxJson = JSON.parse(data);    
+            // var errorCount =  ajaxJson.result.errorcount;
+            // console.log(errorcount);
+            // var timestamp = (new Date).getTime();
+            // var point = [];
+            // point.push(timestamp); 
+            // point.push(errorCount); 
+             //console.log("BEFORE"+point)
+             //var res = point.split("|");
+             //point = res[1];
+             console.log("FINAL"+point);
+             var series = chart.series[0],
+                shift = series.data.length > 20; // shift if the series is longer than 20
+    
+            chart.series[0].addPoint(eval(point), true, shift);
+            
+            setTimeout(requestData,1000);    
+        },
+        cache: false
+    });
+}
+
+$('#viewlive').click(function(){
+        
+        /**
+         * Request data from the server, add it to the graph and set a timeout to request again
+         */
+
+   chart = new Highcharts.Chart({
+                    chart: {
+                        renderTo: 'container',
+                        defaultSeriesType: 'spline',
+                        events: {
+                            load: requestData
+                        }
+                    },
+                    title: {
+                        text: 'Live random data'
+                    },
+                    xAxis: {
+                        type: 'datetime',
+                        tickPixelInterval: 150,
+                        maxZoom: 20 * 1000
+                    },
+                    yAxis: {
+                        minPadding: 0.2,
+                        maxPadding: 0.2,
+                        title: {
+                            text: 'Value',
+                            margin: 80
+                        }
+                    },
+                    series: [{
+                        name: 'Random data',
+                        data: []
+                    }]
+    });        
+});
 $('#query').click(function(){
         var table = $('#example').DataTable();    
          table.destroy();
         var json = $("#input").val();   
-        var topic = "wikipedia";
+        var topic = "test";
         var radios = $(":radio:checked");
         //var checkboxes = $(":checkboxes:checked");
         queryMap = {};
@@ -262,21 +352,23 @@ $('#query').click(function(){
         var temp={};
         temp["type"]="longSum";
         temp["fieldName"]=aggregations;
-        temp["name"]="edit_count";
+        temp["name"]="errorcount";
         queryMap["aggregations"].push(temp);
-        queryMap["dimension"]="page";
-        queryMap["metric"]="edit_count";
+        queryMap["dimension"]="appid";
+        queryMap["metric"]="errorcount";
 
 
         var filterSelected = $( "#filField1 option:selected" ).text();
-    //    var filterSelectedValue = $( "#filFieldInput1").val();
-// debug
-        var filterSelectedValue = "United States"; 
+         //    var filterSelectedValue = $( "#filFieldInput1").val();
+        // debug
+        // var filterSelectedValue = "United States"; 
+        var filterSelectedValue = "L"; 
         filterSelected = filterSelected.trim();
         filterSelectedValue = filterSelectedValue.trim();
         queryMap["filter"]={};
         queryMap["filter"]["type"]="selector";
-        queryMap["filter"]["dimension"]=filterSelected;
+       // queryMap["filter"]["dimension"]=filterSelected;
+        queryMap["filter"]["dimension"]="eventtype";
         queryMap["filter"]["value"]=filterSelectedValue;
 
         console.log(queryMap);
@@ -294,28 +386,76 @@ $('#query').click(function(){
                     data = data.slice(1, - 1);
                     console.log("stringdata "+data);
                     ajaxJson = JSON.parse(data);    
-                   	console.log("TYPE OF OBJECT success"+ajaxJson+"printed json");
+                    console.log("TYPE OF OBJECT success"+ajaxJson+"printed json");
                     console.log("ajaxJsonResult" + ajaxJson.result.length);
-                    var datatablesData =  ajaxJson.result;
+                    datatablesData =  ajaxJson.result;
                     var data2=[];
                     for (i=0; i< datatablesData.length ;i++) {
                         var temp = [];
-                        temp.push(datatablesData[i].edit_count);
-                        temp.push(datatablesData[i].page);
+                        temp.push(datatablesData[i].appid);
+                        temp.push(datatablesData[i].errorcount);
                         data2.push(temp);
                     }
                     console.log(data2);
                     $('#example').dataTable({
                         "aaData": data2,
                         "aoColumns": [
-                            { "sTitle": "edit_count" },
-                            { "sTitle": "page" }
+                            { "sTitle": "appid" },
+                            { "sTitle": "errorcount" }
                         ]
                     }); // datatables end
                 } // success end    
           }); // ajax end
 }); 
 // end of click function
+
+
+
+
+
+$(document).ready(function() {
+    $('#btnAdd').hide();
+    // comment to have additional filter addition
+     $('#btnAdd').click(function() {
+            var num = $('.filterDim').length;
+            var newNum  = new Number(num + 1)
+            var newEl = $('#clonefilter'+num).clone().attr('id','clonefilter'+newNum);
+            newEl.find('#filField'+num).attr('id','filField'+newNum)
+            newEl.find('.input-group').find('#filFieldInput'+num).attr('id','filFieldInput'+newNum)
+            $('.filterDim').append(newEl);
+            //$('.filterDim').after(newEl);
+    });
+});
 </script>
 </body>
 </html>
+
+
+<!--
+
+{
+  "queryType": "topN",
+  "dataSource": "test",
+  "granularity": "all",
+  "dimension": "appid",
+  "metric": "errorcount",
+  "threshold" : 10,
+  "aggregations": [
+    {"type": "longSum", "fieldName": "errorcount", "name": "errorcount"}
+  ],
+  "filter": { "type": "selector", "dimension": "eventtype", "value": "L" },
+  "intervals": ["2012-10-01T00:00/2020-01-01T00"]
+}
+
+[ {
+  "timestamp" : "2015-03-29T20:23:33.000Z",
+  "result" : [ {
+    "errorcount" : 1060,
+    "appid" : "app3"
+  }, {
+    "errorcount" : 584,
+    "appid" : "app1"
+  } ]
+} ]
+-->
+
---
+++ app/live-server-data.php
@@ -0,0 +1,30 @@
+<?php 
+// Set the JSON header
+header("Content-type: text/json");
+
+// The x value is the current JavaScript time, which is the Unix time multiplied by 1000.
+$x = time() * 1000;
+// The y value is a random number
+#$y = rand(0, 100);
+
+// Create a PHP array and echo it as JSON
+#$ret = array($x, $y);
+#echo $ret;
+#echo json_encode($ret);
+
+
+$json=$_POST['json'];
+#$json = '{"queryType":"timeseries","dataSource":"test","granularity":"all","aggregations":[{"type":"longSum","fieldName":"errorcount","name":"errorcount"}],"intervals":["2015-4-3T13:58:10/2015-4-3T13:58:40"]}';
+##$cmd = "curl -X POST 'http://localhost:8084/druid/v2/'  -H 'content-type: application/json' -d'{\"queryType\":\"timeBoundary\",\"dataSource\":\"wikipedia\"}'";
+##$cmd = "curl -X POST 'http://localhost:8084/druid/v2/'  -H 'content-type: application/json' -d'".addcslashes($json, '"')."'";
+$cmd = "curl -X POST 'http://localhost:8084/druid/v2/'  -H 'content-type: application/json' -d'".$json."'";
+$tem = shell_exec($cmd);
+#echo $cmd."|";
+$tem  = json_encode($tem);
+$message = json_decode($tem);
+$t = json_decode($message,true); 
+$y=($t[0]['result']['errorcount']);
+$ret = array($x, $y);
+echo json_encode($ret);
+
+?>
